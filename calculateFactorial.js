19a88b3f4a6185e1520b6abfc3c2fcce335bb6c2
Gulnaz <78388827+gulnazabdykadyrova@users.noreply.github.com>
2024-08-27 06:40:05 +0200

    Update calculateFactorial.js

--- calculateFactorial.js
+++ calculateFactorial.js
@@ -2,6 +2,17 @@
 //lower than it, down to 1 input: 5 output: 120 (54321) 
 
 function calculateFactorial(input){
+  let n = 5;
+function factorial(n) { 
+    if (n === 0) { 
+        return 1; 
+    } 
+    else { 
+        return n * factorial( n - 1 ); 
+    } 
+} 
+
+console.log(factorial(n));
 
 }
 
