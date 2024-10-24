d664ce9a8507515a4c87a0ade85663b2ec7668a7
Gulnaz Abdykadyrova <g.abdykadyrova@gmail.com>
2024-10-24 21:08:33 +0200

    add roman to integer

--- Javascript_probelms/25-longestCommonPrefix.js
+++ Javascript_probelms/25-longestCommonPrefix.js
@@ -1,12 +1,9 @@
 function longestCommonPrefix(strs) {
-  console.log(strs.length);
   if (!strs.length) {
     return "";
   }
   for (let i = 0; i <= strs[0].length; i++) {
-    console.log("i: ", i);
     for (let j = 1; j < strs.length; j++) {
-      console.log("j: ", j);
       if (strs[0][i] !== strs[j][i]) {
         return strs[0].slice(0, i);
       }
