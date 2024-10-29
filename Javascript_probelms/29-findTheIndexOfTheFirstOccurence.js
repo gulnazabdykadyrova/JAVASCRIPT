0b1844b5b9f51a3aa625fb4bffa852d0f883298d
Gulnaz Abdykadyrova <g.abdykadyrova@gmail.com>
2024-10-29 11:16:53 +0100

    first occurence

---
+++ Javascript_probelms/29-findTheIndexOfTheFirstOccurence.js
@@ -0,0 +1,9 @@
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
+// or -1 if needle is not part of haystack.
+
+function firstOccurence(haystack, needle){
+    return haystack.indexOf(needle)
+
+
+}
+console.log(firstOccurence("sadbutsad","but"))
